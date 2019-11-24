9d1071ae236e4b1e5cb4fa3a05710ae5a39f45d4
HitzeD <dakotahhitze@gmail.com>
2019-11-24 16:00:07 -0500

    initial commit

---
+++ src/main/java/net/comcast/x2/X2Application.java
@@ -0,0 +1,15 @@
+package net.comcast.x2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X2Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(X2Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/comcast/x2/X2ApplicationTests.java
@@ -0,0 +1,15 @@
+package net.comcast.x2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X2ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
